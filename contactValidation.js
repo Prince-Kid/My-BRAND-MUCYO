7eee7fd6efe642c16200fb2331257af61299fb91
Prince-Kid <mucyoprinc12@gmail.com>
2024-03-13 23:55:11 +0200

    Adding Js On contact section page For Validation

---
+++ contactValidation.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('form');
+const email = document.getElementById('names');
+const password = document.getElementById('email');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+
+const validateInputs = () => {
+   
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    }
+    else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+   if(emailValue && passwordValue > 8){
+    window.location.href = "admin.html";
+   }
+   
+    
+};
